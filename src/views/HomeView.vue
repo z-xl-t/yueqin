1caf68467e71a6f2d207958033245a2a268efa01
zxlt <z-xl-t@qq.com>
2025-04-07 08:06:48 +0800

    test abcjs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import 'abcjs/abcjs-audio.css'
+import abcjs from 'abcjs'
+
+import { onMounted } from 'vue'
+
+// 定义一个会在组件挂载时调用的函数
+const mountedHandler = () => {
+  abcjs.renderAbc('paper', 'X:1\nK:D\nDD AA|BBA2|\n')
+}
+
+// 使用 onMounted 钩子来调用上面定义的函数
+onMounted(mountedHandler)
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <div id="paper"></div>
   </main>
 </template>
